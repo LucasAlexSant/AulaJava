0ea3fd973ad3a24957417389e526393493241408
LucasAlexSant <lucas.alexsant2@gmail.com>
2024-04-29 13:05:58 -0300

    Como Set testa igualdade

--- Set_01/src/application/Program.java
+++ Set_01/src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -8,6 +9,19 @@ public class Program {
 
 	public static void main(String[] args) {
 
+		Set<String> set = new HashSet<>();
+		set.add("TV");
+		set.add("Notebook");
+		set.add("Tablet");
+		
+		System.out.println(set.contains("Notebook"));
+		
+		for (String p : set) {
+		System.out.println(p);
+		}
+		
+		System.out.println(); 
+		
 		Set<Integer> a = new TreeSet<>(Arrays.asList(0,2,4,5,6,8,10));
 		Set<Integer> b = new TreeSet<>(Arrays.asList(5,6,7,8,9,10));
 		
